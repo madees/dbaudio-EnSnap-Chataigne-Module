921025b819dad06f741998f34e10c3a0c9c398fb
Mathieu Delquignies <67159179+madees@users.noreply.github.com>
2021-02-12 22:46:36 +0100

    Add files via upload

---
+++ EnSnap-OSC.js
@@ -0,0 +1,60 @@
+/* Chataigne Module for d&b audiotechnik/Gareth Owen EnSnap software OSC control v1.1.1 (c) Mathieu Delquignies, 02/2021
+===============================================================================
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED "AS IS" AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+===============================================================================
+*/
+
+/**
+ * This function is called automatically by Chataigne when you add the module in your Noisette.
+ */
+function init() 
+{
+	local.scripts.setCollapsed(true);
+	local.templates.setCollapsed(true);
+}
+
+/**
+ * Enables or disables OSC cue firing controls.
+ * * @param {boolean} state
+ */
+function OSCCueControl(state)
+{
+	local.send("/ensnap/enables/OscCueControl", state);
+}
+
+/**
+ * Re-enables both OSC and MTC cue firing controls if they have been previously disabled.
+ */
+function enableAllCue()
+{
+	local.send("/ensnap/enables/AllCueControl");
+}
+
+/**
+ * Set EnSpace reverb predelay factor
+ * @param {string} name 
+ */
+function goCue(name)
+{
+	local.send("/ensnap/cue/"+name+"/go");
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# dbaudio-EnSnap-Chataigne-Module
+Chataigne module to control d&amp;b audiotechnik/Gareth Owen EnSnap software with OSC protocol.  
+
+To learn more about Chataigne, please visit : http://benjamin.kuperberg.fr/chataigne/
+
+And Ben's Youtube channel where you can find tutorials : https://youtu.be/RSBU9MwJNLY
+
+About EnSnap software : https://www.dbaudio.com/global/en/about-db/press/newsroom/db-audiotechnik-partners-with-gareth-owen-sound-to-advance-soundscape/
+To learn more about d&amp;b audiotechnik Soundscape, please visit : https://www.dbsoundscape.com/  
+
+For global support on how to use Chataigne and its modules, please visit the forum : 
+http://benjamin.kuperberg.fr/chataigne/forum 
+or join us on Discord : 
+https://discord.com/invite/ngnJ5z my contact there is also "madees".
+
+## Installation
+To install the Custom Module, unzip the folder to your Documents/Chataigne/Modules folder.
+
+## Principle of use
+You may use Module Commands to control EnSnap software.
+This module is output only.
+
